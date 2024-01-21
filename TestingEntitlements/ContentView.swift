efeb6013c9e320cfb8c336ae819b521c644c88bf
Michael Lustig <lustig@knophy.com>
2024-01-21 08:49:21 -0500

    Refactored ContentView and added OpenedAppsViewModel and OpenedAppsView.

--- TestingEntitlements/ContentView.swift
+++ TestingEntitlements/ContentView.swift
@@ -1,19 +1,31 @@
 //
 //  ContentView.swift
-//  TestingEntitlements
+//  YouveBeenFramed
 //
-//  Created by laptop on 1/21/24.
+//  Created by laptop on 1/20/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var identity = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(identity)
+                .font(.largeTitle)
+                .onAppear {
+                    let fileManager = FileManager.default
+                  print("hi i appeared in the view")
+                    let groupURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.com.knophy.mybroadcast")!
+                    let fileURL = groupURL.appendingPathComponent("identity.txt")
+
+                    do {
+                        identity = try String(contentsOf: fileURL, encoding: .utf8)
+                    } catch {
+                        print("Failed to read identity: \(error)")
+                    }
+                }
         }
         .padding()
     }
@@ -21,4 +33,47 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+import Foundation
+import Combine
+
+class OpenedAppsViewModel: ObservableObject {
+    @Published var openedApps: [String] = []
+
+    private var cancellables = Set<AnyCancellable>()
+    private let userDefaults: UserDefaults
+
+    init(appGroup: String) {
+        guard let userDefaults = UserDefaults(suiteName: appGroup) else {
+            fatalError("Could not initialize UserDefaults with app group: \(appGroup)")
+        }
+        self.userDefaults = userDefaults
+
+        // Observing the changes in UserDefaults
+        userDefaults
+            .publisher(for: \.openedAppsKey, options: [.initial, .new])
+            .compactMap { $0 }
+            
+            .assign(to: &$openedApps)
+    }
+}
+
+struct OpenedAppsView: View {
+    @ObservedObject var viewModel: OpenedAppsViewModel
+
+    var body: some View {
+        NavigationView {
+            List(viewModel.openedApps, id: \.self) { app in
+                Text(app)
+            }
+            .navigationBarTitle("Opened Applications")
+        }
+    }
+}
+
+extension UserDefaults {
+    @objc dynamic var openedAppsKey: [String]? {
+        return array(forKey: "openedApps") as? [String]
+    }
 }
