848c2742eef6794f29f3c4c122db046f786b06e2
Michael Lustig <lustig@knophy.com>
2024-01-21 08:44:07 -0500

    Initial Commit

---
+++ TestingEntitlements/TestingEntitlementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingEntitlementsApp.swift
+//  TestingEntitlements
+//
+//  Created by laptop on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingEntitlementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
